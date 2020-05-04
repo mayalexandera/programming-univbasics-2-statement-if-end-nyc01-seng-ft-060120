a681e69f460f63fe4accc6800f45c97cb20694ed
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 17:11:21 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,4 @@
 # Write your solution here
 run_code_inside = TRUE
 
-current_time = Time.now.to_i
-
- 
-
-current_time.even? ? (puts "Even!") : (puts "Odd!")
+Time.now.to_i.even? ? (puts "Even!") : (puts "Odd!")
